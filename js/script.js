88622269e0fe5ae135d7b48d0ad1887eab0a3e33
biruk1122 <birukfikadu29@gmail.com>
2024-11-20 18:34:16 +0300

    Implementing smooth scrolling

--- js/script.js
+++ js/script.js
@@ -13,3 +13,30 @@ const headerEl = document.querySelector(".header")
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open")
 })
+
+//Smooth Scrolling
+const allLinks = document.querySelectorAll("a:Link")
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault()
+    const href = link.getAttribute("href")
+
+    //Scrol back to the top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      })
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href)
+      sectionEl.scrollIntoView({ behavior: "smooth" })
+    }
+
+    //Close Mobile Navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open")
+  })
+})
